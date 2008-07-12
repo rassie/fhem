5b767febcdd72a6bb41a368ea707a5c8973d5d08
nobody <nobody@2b470e98-0d58-463d-a4d8-8e2adae1ed80>
2008-07-12 09:17:40 +0000

    This commit was manufactured by cvs2svn to create tag 'FHEM_4_3'.

---
+++ tags/FHEM_4_3/fhem/FHEM/98_dummy.pm
@@ -0,0 +1,47 @@
+##############################################
+package main;
+
+use strict;
+use warnings;
+
+sub
+dummy_Initialize($)
+{
+  my ($hash) = @_;
+
+  $hash->{SetFn}     = "dummy_Set";
+  $hash->{DefFn}     = "dummy_Define";
+}
+
+###################################
+sub
+dummy_Set($@)
+{
+  my ($hash, @a) = @_;
+
+  return "no set value specified" if(int(@a) != 2);
+  return "Unknown argument $a[1], choose one of *" if($a[1] eq "?");
+
+
+  my $v = $a[1];
+
+  Log GetLogLevel($a[0],2), "dummy set $v";
+
+  $hash->{CHANGED}[0] = $v;
+  $hash->{STATE} = $v;
+  $hash->{READINGS}{state}{TIME} = TimeNow();
+  $hash->{READINGS}{state}{VAL} = $v;
+  return undef;
+}
+
+sub
+dummy_Define($$)
+{
+  my ($hash, $def) = @_;
+  my @a = split("[ \t][ \t]*", $def);
+
+  return "Wrong syntax: use define <name> dummy" if(int(@a) != 2);
+  return undef;
+}
+
+1;
